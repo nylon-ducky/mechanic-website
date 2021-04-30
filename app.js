2137ed07992d8420663162ddcc0ec0845c152da0
Durso <dursozd@gmail.com>
2021-04-30 14:26:51 -0400

    responsive design progress

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 function openNav() {
     let navBar = document.querySelector("#navBar");
+    if (screen.width <= "674px") {
+        if (navBar.style.visibility == "visible") {
+            navBar.style.visibility = "collapse";
+        }
 
-    if (navBar.style.visibility == "visible") {
-        navBar.style.visibility = "collapse";
+        else {
+            navBar.style.visibility = "visible";
+        }
     }
+    else { return }
 
-    else {
-        navBar.style.visibility = "visible";
-    }
 }
