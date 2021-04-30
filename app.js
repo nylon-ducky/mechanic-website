9aa83b42149757074f3f5137ace5d93463691e5f
Durso <dursozd@gmail.com>
2021-04-30 14:44:07 -0400

    nav-bar fix

--- app.js
+++ app.js
@@ -1,14 +1,26 @@
 function openNav() {
     let navBar = document.querySelector("#navBar");
-    if (screen.width <= "674px") {
+
+    let screenWidth = screen.width;
+    console.log(screenWidth);
+
+    if (screenWidth <= 674) {
         if (navBar.style.visibility == "visible") {
-            navBar.style.visibility = "collapse";
+            navBar.style.visibility = "hidden";
+        }
+
+        else if (navBar.style.visibility == "hidden") {
+            navBar.style.visibility = "visible";
         }
 
         else {
             navBar.style.visibility = "visible";
         }
     }
-    else { return }
+
+    else {
+        return;
+    }
+    
 
 }
