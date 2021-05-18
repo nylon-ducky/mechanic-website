bfffac1e19d305f7e9b121826b4a73305109566e
Durso <dursozd@gmail.com>
2021-05-18 17:18:15 -0400

    heroku

---
+++ form.php
@@ -0,0 +1,20 @@
+<?php
+
+if(isset($_POST['submit'])) {
+    $fname = $_POST['fname'];
+    $lname = $_POST['lname'];
+    $phone = $_POST['phone'];
+    $email = $_POST['email'];
+    $makemodel = $_POST['makemodel'];
+    $year = $_POST['year'];
+    $notes = $_POST['notes'];
+    $datepicker = $_POST['date-picker'];
+
+    $mailto = "radridesstudio@gmail.com";
+    $header = $fname . " " . $lname . " Would like to book an appointment.\n\n See details below.";
+
+    $content = " $fname $lname \n $phone $email \n $makemodel $year \n $notes \n preferred drop off date and time: $datepicker";
+
+    mail($mailto, $header, $content);
+
+}
